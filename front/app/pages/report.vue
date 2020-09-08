d3f1f5365c3696c0e29ac713dc3b7f6fd4774285
haise578 <gffeva+@gmail.com>
2020-09-09 08:24:26 +0900

    :+1:レポート提出フォーム作成

--- front/app/pages/report.vue
+++ front/app/pages/report.vue
@@ -1,38 +1,65 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col cols="12" sm="6">
-        <v-textarea
-          class="mx-2"
-          label="prepend-icon"
-          rows="1"
-          prepend-icon="mdi-comment"
-        ></v-textarea>
-      </v-col>
-      <v-col cols="12" sm="6">
-        <v-textarea
-          append-icon="mdi-comment"
-          class="mx-2"
-          label="append-icon"
-          rows="1"
-        ></v-textarea>
-      </v-col>
-      <v-col cols="12" sm="6">
-        <v-textarea
-          prepend-inner-icon="mdi-comment"
-          class="mx-2"
-          label="prepend-inner-icon"
-          rows="1"
-        ></v-textarea>
-      </v-col>
-      <v-col cols="12" sm="6">
-        <v-textarea
-          append-outer-icon="mdi-comment"
-          class="mx-2"
-          label="append-outer-icon"
-          rows="1"
-        ></v-textarea>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div>
+    <v-container v-for="(report, index) in reportList" :key="index">
+      <v-card>
+        <v-row>
+          <v-col cols="4" sm="2">
+            <v-select
+              v-model="report.time"
+              :items="hoursList"
+              label="時間"
+              class="mx-2"
+            ></v-select>
+          </v-col>
+          <v-col cols="4" sm="2">
+            <v-select
+              v-model="report.type"
+              :items="typeList"
+              class="mx-2"
+            ></v-select>
+          </v-col>
+          <v-col cols="4" sm="8">
+            <v-textarea
+              v-model="report.content"
+              class="mx-2"
+              label="内容"
+              rows="1"
+            ></v-textarea>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-container>
+    <button @click="addRecord">追加</button>
+  </div>
 </template>
+
+<script>
+import ReportRecord from '~/components/ReportRecord.vue'
+const maxHour = 12
+const hourRange = [...Array(maxHour).keys()]
+
+export default {
+  data() {
+    return {
+      reportList: [],
+      typeList: ['勉強', 'ゲーム', 'ご飯'],
+      hoursList: hourRange,
+    }
+  },
+  created() {
+    this.reportList = [this.initReport()]
+  },
+  methods: {
+    addRecord() {
+      this.reportList.push(this.initReport())
+    },
+    initReport() {
+      return {
+        content: '',
+        type: '勉強',
+        time: 0,
+      }
+    },
+  },
+}
+</script>
