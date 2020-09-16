afdb197e27e793695b47c2280f1cc8340f16cea0
haise578 <gffeva+@gmail.com>
2020-09-16 09:00:45 +0900

    :+1:Add test page

---
+++ front/app/pages/test.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <h1>{{ message }}</h1>
+  </v-container>
+</template>
+
+<script>
+export default {
+  asyncData({ $axios, params }) {
+    return $axios.$get(`http://back:8080/test`).then((res) => {
+      return { message: res.message }
+    })
+  },
+}
+</script>
